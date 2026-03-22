f33dee90b81ebd220639ae8ed89203d6352d21c9
Gregg Luskin <5702322+luskin@users.noreply.github.com>
2026-03-22 10:49:10 -0700

    Add durabull cask v1.3.0

---
+++ Casks/durabull.rb
@@ -0,0 +1,26 @@
+cask "durabull" do
+  version "1.3.0"
+  sha256 "bd095e804ce495ea192c213dd79f856e0497c10a6ff6bf80d9d1163282eff754"
+
+  url "https://github.com/durabullhq/durabull/releases/download/v#{version}/Durabull-#{version}-arm64.dmg",
+      verified: "github.com/durabullhq/durabull/"
+
+  name "Durabull"
+  desc "The modern dashboard for BullMQ"
+  homepage "https://durabull.io"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on arch: :arm64
+
+  app "Durabull.app"
+
+  zap trash: [
+    "~/Library/Application Support/Durabull",
+    "~/Library/Caches/com.durabull.desktop",
+    "~/Library/Preferences/com.durabull.desktop.plist",
+  ]
+end
